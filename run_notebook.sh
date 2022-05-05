9231bb0310e8b5eb32c3ccbe1168145f29acb8f5
Konrad Ponichtera <konpon96@gmail.com>
2022-05-05 17:12:03 +0200

    Docker Compose for avoiding local directory mounting

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-DOCKER_IMAGE=cs4035
+# Docker Compose wrapper
 
-docker build -t $DOCKER_IMAGE .
+export CONTAINER_USER="$(id -u):$(id -g)"
+export PROJECT_DIR="$(pwd)"
 
-docker run -it \
-  -p 127.0.0.1:8888:8888 \
-  -w /jupyter \
-  -u "$(id -u):$(id -g)" \
-  -v "$HOME/.local/share/jupyter:/.local/share/jupyter" \
-  -v "$HOME/.config/matplotlib:/.config/matplotlib" \
-  -v "$HOME/.cache/matplotlib:/.cache/matplotlib" \
-  -v "$HOME/.cache/jedi:/.cache/jedi" \
-  -v "$HOME/.jupyter:/.jupyter" \
-  -v "$(pwd)/jupyter:/jupyter" \
-  $DOCKER_IMAGE
+docker compose "$@"
